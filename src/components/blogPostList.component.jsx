65ca8d27823fcada1d1ed87e722a4079e2371e2a
tejasvi <tejshinde9876@gmail.com>
2023-04-28 08:02:34 +0530

    fix: blog list changes and css improvements added

--- src/components/blogPostList.component.jsx
+++ src/components/blogPostList.component.jsx
@@ -5,68 +5,92 @@ import BlogPost from './blogpost/blogPost.component'
 const BlogPostList = () => {
 
   let [blogPost, setBlogPost] = useState([]);
-  
 
-  useEffect(()=>{
+
+  useEffect(() => {
     blogPost = [
       {
-      "id": 1,
-      "isParent":true,
-      "date": 'April 11, 2023',
-      "title": "A Trip to Japan",
-      "content": "<p>Welcome to my blog! This is my first post. Lorem ipsum dolor sit amet, consectetur <a href='data1'>adipiscing </a> elit. Nullam vitae est sapien. Phasellus dignissim, ipsum in bibendum varius, ipsum nulla maximus erat, ac dapibus turpis velit in est. Sed molestie lectus et dolor <a href='data'>lobortis</a> euismod. Nullam vel nibh nec purus cursus efficitur. Donec convallis diam ac diam consectetur bibendum. Duis auctor, nunc at convallis aliquam, tellus dolor placerat nunc, vel maximus est lorem non dolor. Sed a dolor nec tortor facilisis iaculis at vel lectus. Sed vel lectus massa. Donec vitae eleifend sapien. Etiam in purus augue. Nullam convallis malesuada dolor quis gravida. Suspendisse potenti. Proin rutrum erat non justo facilisis, a vulputate enim dictum. Thank you for reading!</p>",
-      "child_posts": [
-        {
-          "id": "data1",
-          "isParent": false,
-          "title": "Exploring Tokyo",
-          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-          "img": ''
-        },
-        {
-          "id": "data",
-          "isParent": false,
-          "title": "Visiting Kyoto",
-          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-          "img": ''
-        },
-        {
-          "title": "Trying Japanese cuisine",
-          "isParent": false,
-          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-          "img": ''
+        "id": 1,
+        "isParent": true,
+        "date": "April 11, 2023",
+        "title": "A Trip to Japan",
+        "content": "<p>Welcome to my blog! This is my first post. Lorem ipsum dolor sit amet, consectetur <a href='data1' className='child-post-link'>adipiscing </a> elit. Nullam vitae est sapien. Phasellus dignissim, ipsum in bibendum varius, ipsum nulla maximus erat, ac dapibus turpis velit in est. Sed molestie lectus et dolor <a href='data'>lobortis</a> euismod. Nullam vel nibh nec purus cursus efficitur. Donec convallis diam ac diam consectetur bibendum. Duis auctor, nunc at convallis aliquam, tellus dolor placerat nunc, vel maximus est lorem non dolor. Sed a dolor nec tortor facilisis iaculis at vel lectus. Sed vel lectus massa. Donec vitae eleifend sapien. Etiam in purus augue. Nullam convallis malesuada dolor quis gravida. Suspendisse potenti. Proin rutrum erat non justo facilisis, a vulputate enim dictum. Thank you for reading!</p>",
+        "child_posts": [
+          {
+            "id": "data1",
+            "isParent": false,
+            "title": "Exploring Tokyo",
+            "content": "Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.",
+            "img": " "
+          },
+          {
+            "id": "data",
+            "isParent": false,
+            "title": "Visiting Kyoto",
+            "content": "Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.",
+            "img": ""
+          },
+          {
+            "title": "Trying Japanese cuisine",
+            "isParent": false,
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            "img": ""
+          }
+        ],
+        "citation": {
+          "cited_by": [
+            {
+              "description": "Huynen J, McCall R and Griffin M. Towards design recommendations for training of security critical agents in mixed reality environments. Proceedings of the 32nd International BCS Human Computer Interaction Conference. (1-5).",
+              "link": "https://doi.org/10.14236/ewic/HCI2018.84"
+            },
+            {
+              "description": "  K, Peever N, Klarkowski M, Ploderer B, Mitchell J and Johnson D. Using Applied Games to Engage mHealth Users. Proceedings of the 2018 Annual Symposium on Computer-Human Interaction in Play. (511-522).",
+              "link": "https://doi.org/10.1145/3242671.3242686"
+            },
+            {
+              "description": "Cheng K, Lin V, Nijhawan K, Westhem A and Bernstein M. Apps with Benefits. Proceedings of the 2017 CHI Conference Extended Abstracts on Human Factors in Computing Systems. (2452-2458)",
+              "link": "https://doi.org/10.1145/3027063.3053276"
+            }
+          ]
         }
-      ]
-    },
-    {
-      id: 2,
-      title: 'My second Blog Post',
-      date: 'April 11, 2023',
-      "isParent": true,
-      content: `
-        Welcome to my blog! This is my first post.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae est sapien. Phasellus dignissim, ipsum in bibendum varius, ipsum nulla maximus erat, ac dapibus turpis velit in est. Sed molestie lectus et dolor lobortis euismod. Nullam vel nibh nec purus cursus efficitur. Donec convallis diam ac diam consectetur bibendum. Duis auctor, nunc at convallis aliquam, tellus dolor placerat nunc, vel maximus est lorem non dolor. Sed a dolor nec tortor facilisis iaculis at vel lectus. Sed vel lectus massa. Donec vitae eleifend sapien. Etiam in purus augue. Nullam convallis malesuada dolor quis gravida. Suspendisse potenti. Proin rutrum erat non justo facilisis, a vulputate enim dictum.
-        Thank you for reading!
-      `
-    }
-  ];
-    setBlogPost(blogPost);
+      },
+      {
+        "id": 2,
+        "title": "My second Blog Post",
+        "date": "April 11, 2023",
+        "isParent": true,
+        "content": "Welcome to my blog! This is my first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae est sapien. Phasellus dignissim, ipsum in <a href='data3'>bibendum</a> varius, ipsum nulla maximus erat, ac dapibus turpis velit in est. Sed molestie lectus et dolor lobortis euismod. Nullam vel nibh nec purus cursus efficitur. Donec convallis diam ac diam consectetur bibendum. Duis auctor, nunc at convallis aliquam, tellus dolor placerat nunc, vel maximus est lorem non dolor. Sed a dolor nec tortor facilisis iaculis at vel lectus. Sed vel lectus massa. Donec vitae eleifend sapien. Etiam in purus augue. Nullam convallis malesuada dolor quis gravida. Suspendisse potenti. Proin rutrum erat non justo facilisis, a vulputate enim dictum. Thank you for reading! ",
+        "child_posts": [
+          {
+            "id": "data3",
+            "isParent": false,
+            "title": "Exploring Anime",
+            "content": "Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.Anand is the administrative centre of Anand District in the state of Gujarat, India. It is administered by Anand Municipality. It is part of the region known as Charotar, consisting of Anand and Kheda districts.",
+            "img": ""
+          }
+        ]
+      }
+    ]
+setBlogPost(blogPost);
   }, [])
-  
- const toggleShowHide = () => {
-   console.log('clickeddd')
-  }
 
-  const post = blogPost.map((post) => (
-    <BlogPost key={post.id} post={post} onChildEvent={toggleShowHide}  />
-  ));
-  
-  
-  return (
+const toggleShowHide = () => {
+  console.log('clickeddd')
+}
+
+const post = blogPost.map((post) => (
+  <BlogPost key={post.id} post={post} onChildEvent={toggleShowHide} />
+));
+
+
+return (
+  <>
+    <div className="list-title">Wikipedia Article: JAPAN</div>
     <div className="post-list">
       {post}
     </div>
-  );
+  </>
+);
 };
 
 export default BlogPostList;
--- src/components/blogpost/blogPost.component.jsx
+++ src/components/blogpost/blogPost.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './blogPost.scss';
 import ReactHtmlParser from 'html-react-parser';
 import Popup from 'reactjs-popup';
@@ -6,20 +6,29 @@ import 'reactjs-popup/dist/index.css';
 // import CommentList from './CommentList';
 
 const BlogPost = (props) => {
-  const { post }  = props;
+  const { post } = props;
+  const [show, setShow] = useState(false);
+  const [childPosts, setChildPosts] = useState({});
 
-  const handleLinkClick = (event, href) => {
+  const handleLinkClick = (event, docId) => {
     event.preventDefault();
-    console.log('Link clicked:', href);
+    console.log('Link clicked:', docId);
+    console.log({post})
+    const childPost = post.child_posts.find((item)=> item.id == docId);
+    console.log({childPost})
+    setChildPosts(childPost)
+
+
+    setShow(true)
     props.onChildEvent(event);
   }
 
   const options = {
     replace: (domNode) => {
       if (domNode.name === 'a' && domNode.attribs && domNode.attribs.href) {
-        const href = domNode.attribs.href;
+        const docId = domNode.attribs.href;
         return (
-          <a href={href} onClick={(e) => handleLinkClick(e, href)}>
+          <a href={docId} onClick={(e) => handleLinkClick(e, docId)}>
             {domNode.children[0].data}
           </a>
         );
@@ -27,19 +36,32 @@ const BlogPost = (props) => {
     }
   };
 
- const handleClick = (event) => {
-    console.log('on childd')
+  const onClosePopup = () => {
+    setShow(false);
   }
 
   return (
     <div className="post">
-      <h2 className="title">{post.title}</h2>
-      <p className="body">{ReactHtmlParser(post.content, options)}</p>
-      <button onClick={()=> handleClick('test')}>test</button>
-      <Popup trigger={<button> Trigger</button>} position="right center" modal>
-        <div>cursus efficitur. Donec convallis diam ac diam consectetur bibendum. Duis auctor, nunc at convallis aliquam, tellus dolor placerat nunc, vel maximus est lorem non dolor. Sed a dolor nec tortor facilisis iaculis at vel lectus. Sed vel lectus massa. Donec vitae eleifend sapien. Etiam in purus augue. Nullam convallis malesuada dolor quis gravida. Suspendisse potenti. 
-            Proin rutrum erat non justo facilisis, a vulputate enim dictum. Thank you for reading!</div>
-    </Popup>
+      <h2 className="title">{post.title}</h2>  
+      <span className="body">{ReactHtmlParser(post.content, options)}</span>
+      <Popup open={show} position="right center" onClose={onClosePopup} modal>
+        {close => (
+          <div>
+            <div className="post">
+            <div className='header-flex'>
+              <h2 className="title">{childPosts.title}</h2>
+              <span className="close" onClick={close}>
+              X
+            </span>
+              </div>
+              <span className="body">{childPosts.content}
+              </span>
+            </div>
+            
+          </div>
+        )}
+
+      </Popup>
       {/* <CommentList comments={post.comments} /> */}
     </div>
   );
